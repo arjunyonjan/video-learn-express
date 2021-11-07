09399c79d621e43bcd10f2b0254e911930808bd2
Arjun Yonjan <arjun.yonzan@gmail.com>
2021-11-07 16:42:21 +0545

    commit

--- start.bat
+++ start.bat
@@ -1,3 +1,5 @@
 "C:\Program Files\Google\Chrome\Application\chrome_proxy.exe"  --profile-directory="Profile 1" --app-id=ofhbbkphhbklhfoeikjpcbhemlocgigb
 
 START nodemon
+
+START https://videolearn.netlify.app
